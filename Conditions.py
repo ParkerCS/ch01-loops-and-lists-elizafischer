4100c2045b6aba7a43fdc12eaa9451b2718a7c23
elizafischer <efischer@fwparker.org>
2017-02-03 16:28:07 -0600

    Done

--- Conditions.py
+++ Conditions.py
@@ -45,6 +45,7 @@ def grader(percent):
 percent = float(input("Enter a grade percentage and the computer will tell you your letter grade:"))
 grader(percent)
 
+print("")
 # PROBLEM 2 (Vowels - 5pts) ***
 # Ask the user to supply a string.
 # Print how many different vowels there are in the string.
@@ -53,8 +54,7 @@ grader(percent)
 # Try to print proper output (e.g., printing “There are 1 different vowels in the string” is ugly).
 # Example: When the user enters the string “It’s Owl Stretching Time,”
 # the program should say that there are 3 different vowels in the string.
-
-
+'''
 def find_vowel(user):
     vowel_count = 0
     if user.find("a"):
@@ -74,8 +74,22 @@ def find_vowel(user):
 find_vowel(input(str("Enter a string and the computer will tell you how many different vowels there are:")))
 #print("There are " , vowel_count , "different vowels in this string. They are," , vowel)
 
+Doesn't work
+'''
+
+user = (str(input("Enter a string and the computer will tell you how many different vowels there are: ")))
+total_vowels = []
+for vowel in ["a", "e", "i", "o", "u"]:
+    if vowel in user:
+        total_vowels.append(vowel)
+
+if len(total_vowels)== 1:
+    print("There is 1 vowel in your phrase.")
+else:
+   print("There are", len(total_vowels), "different vowels in this string.")
 
 
+print("")
 # PROBLEM 3 (Quadratic Equation - 6pts) ****
 # You can solve quadratic equations using the quadratic formula.
 # Quadratic equations are of the form Ax2 + Bx + C = 0.
@@ -105,5 +119,4 @@ else:
     print("Solution 2 = ", ((b * -1) - ((sqrt((b**2) - (4*(a)*(c)) ))/ (2 * a) )))
 if value == 0:
     print("There is One solution")
-    solution1 = ((- b +(b ** 2) - 4*(a)*(c))) / 2*a
-    print
+    solution1 = (((- b +(b ** 2) - 4*(a)*(c))) / 2*a)
